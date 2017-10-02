56f8c1f9ed9e0a1987ccce7c518481deb495ea33
averyg21 <averygrimes@hotmail.com>
2017-10-02 17:30:38 -0500

    Added a new class.

---
+++ app/src/main/java/com/example/averygrimes/helloworld/Check.java
@@ -0,0 +1,52 @@
+package com.example.averygrimes.helloworld;
+
+/**
+ * Created by Avery Grimes on 9/30/2017.
+ */
+
+public class Check {
+
+    /**
+     * Variable to hold the number
+     */
+    private int num;
+
+    /**
+     * Variable to hold the name
+     */
+    private String name;
+
+    /**
+     * get method to return the number
+     * That the class holds
+     *
+     * @return num
+     */
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set method to change the name
+     * of the instance variable "name"
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void check(){
+
+    }
+
+}
